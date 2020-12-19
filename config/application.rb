301c0378e59eec975669344a1a0659480c0fd455
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2020-12-20 02:53:22 +0900

    言語設定ファイルの修正

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,8 @@ Bundler.require(*Rails.groups)
 
 module AppName
   class Application < Rails::Application
-    config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
-
     # デフォルトのlocalを日本語(:ja)にする
-    config.i18n,default_locale = :ja
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
