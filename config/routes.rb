320845481fd15a4e758eda5d87b1a15387909c08
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2020-12-19 18:22:52 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit]
 
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
