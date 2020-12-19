458b1cbe0e77e26995207de35b594fbe7d1fc29a
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2020-12-19 18:38:51 +0900

    usersコントローラーにupdateアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:info] = "プロフィールが更新されました！"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  # プロフィール編集時に許可する属性
+  def user_params
+    params.require(:user).permit(:name, :email, :introduction)
+  end
 end
