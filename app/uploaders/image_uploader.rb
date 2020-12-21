418941cf0331df5e414e94abbdc5a0ee5fa45e8c
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2020-12-22 00:56:48 +0900

    画像のリサイズと拡張子の指定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -11,6 +11,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # 画像の上限を400pxにする
+  process :resize_to_limit => [400, 400]
+
+  # jpg,jpeg,pngしか受け付けない
+  def extension_white_list
+    %w(jpg jpeg png)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
