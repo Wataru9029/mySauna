3d150366f0eedf22fb338f514d462764ebd75695
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2020-12-23 19:34:01 +0900

    新規投稿機能の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,21 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new
+    if @post.update_attributes(post_params)
+      flash[:info] = "新規記事が投稿されました！"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  # 記事投稿時に許可する属性
+  def post_params
+    params.require(:post).permit(:title, :image, :address, :description, :site_url)
+  end
 end
