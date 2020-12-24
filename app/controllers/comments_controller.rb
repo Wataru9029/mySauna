5ba2bf4c7f24939a55125d460ae8f08024540a4d
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2020-12-24 23:33:01 +0900

    コントローラーのアクション作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @post = Post.find(params[:id])
+    if @comment.save
+        flash[:info] = "新規コメントが投稿されました！"
+    else
+        flash[:danger] = "コメント投稿に失敗しました！"
+    end
+    redirect_to @post
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
