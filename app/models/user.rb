c45452a438f2de39d5995e39d5c95eb76b450734
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2020-12-26 18:48:20 +0900

    Likeモデルとの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
 
   mount_uploader :image, ImageUploader
   # Include default devise modules. Others available are:
