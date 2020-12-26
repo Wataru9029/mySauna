5942ef6491577d58dbe9fa00fe856ee968332aef
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2020-12-26 21:13:47 +0900

    非ログイン時でも人気記事一覧を表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:show, :search]
+  before_action :authenticate_user!, except: [:show, :search, :rank]
 
   def index
     if params[:tag_name]
