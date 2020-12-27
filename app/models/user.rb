e82125c18b1caa0ed5d903ffb9e562b065254389
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2020-12-27 23:45:08 +0900

    フォロー機能のメソッド追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,19 @@ class User < ApplicationRecord
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
   end
+
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 end
