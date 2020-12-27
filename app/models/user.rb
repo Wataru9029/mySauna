5b57b546d8b52e7056d7974534d676491ced2276
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2020-12-27 23:38:33 +0900

    RelationshipモデルとUserモデルの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
 
   mount_uploader :image, ImageUploader
   # Include default devise modules. Others available are:
