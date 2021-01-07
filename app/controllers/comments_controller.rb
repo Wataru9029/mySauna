f439cbcfebef64a0c171a30c9508a5d9a07301ee
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2021-01-07 23:06:03 +0900

    コメント作成&削除後に前のページにリダイレクト

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,18 +8,18 @@ class CommentsController < ApplicationController
     else
         flash[:danger] = "コメント投稿に失敗しました！"
     end
-    redirect_to root_url
+    redirect_to request.referrer || root_url
   end
 
   def destroy
     @comment = Comment.find(params[:id])
-    @post = Post.find(params[:id])
+    @post = @comment.post
     if @comment.user_id == current_user.id && @comment.destroy
         flash[:info] = "コメントが削除されました！"
     else
         flash[:danger] = "コメント削除に失敗しました！"
     end
-    redirect_to root_url
+    redirect_to post_url(@post)
   end
 
   private
