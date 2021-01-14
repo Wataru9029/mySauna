532a188ec434938592e9690734d065c185316f05
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2021-01-14 16:47:28 +0900

    認証機能周りのメソッドをRequestテストで使えるようにする

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,8 +20,6 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
-
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
@@ -62,4 +60,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+  Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
+  config.include RequestSpecHelper, type: :request
 end
