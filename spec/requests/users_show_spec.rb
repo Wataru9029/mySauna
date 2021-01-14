6b8ba4490dafa8eba1e708ddd6a8b362eaa16fa0
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2021-01-14 21:27:26 +0900

    プロフィールページのRequestテスト作成

---
+++ spec/requests/users_show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "プロフィールページ", type: :request do
+    before do
+        @user = create(:user)
+     end
+
+  context "トップページ" do
+    it "レスポンスが正常に表示されること" do
+        sign_in @user
+        get user_path(@user)
+        expect(response).to render_template('users/show')
+    end
+  end
+end
