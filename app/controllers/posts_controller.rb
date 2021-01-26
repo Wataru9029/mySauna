db151cd753a6fd7fa8506ea6366fd3c11eb307b9
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2021-01-26 19:55:05 +0900

    余計な空行の削除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :search, :rank, :rate, :infection_control]
   before_action :correct_user, only: [:edit, :update, :destroy]
 
-
   def index
     if params[:tag_name]
       @posts = Post.tagged_with(params[:tag_name].to_s)
