726670a07e7446977f10515fc01455a8ac5dc12f
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2021-01-26 19:04:06 +0900

    インデント修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    PER = 5
+  PER = 5
 
-    protected
+  protected
 
-    # 入力フォームからアカウント名情報をDBに保存するために追加
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    end
+  # 入力フォームからアカウント名情報をDBに保存するために追加
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
