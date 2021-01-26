65bd3f9ede67c047633d5a54a1398ed6ce1ae0d6
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2021-01-26 19:45:07 +0900

    バリデーション修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 1200 }
-  validates :site_url, format: /\A#{URI::regexp(%w[http https])}\z/, if: Proc.new { |post| post.site_url.present? }
+  validates :site_url, format: /\A#{URI::regexp(%w[http https])}\z/, if: proc { |post| post.site_url.present? }
   validates :rate, numericality: {
     less_than_or_equal_to: 5,
     greater_than_or_equal_to: 1
@@ -17,7 +17,7 @@ class Post < ApplicationRecord
   validates :infection_control_rate, numericality: {
     less_than_or_equal_to: 5,
     greater_than_or_equal_to: 1
-  }, if: Proc.new { |post| post.infection_control_rate.present? }
+  }, if: proc { |post| post.infection_control_rate.present? }
 
   acts_as_taggable
   geocoded_by :address
