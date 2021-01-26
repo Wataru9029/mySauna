23ead351b86747cd677301a11cf5a7f100094391
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2021-01-26 18:41:11 +0900

    ハッシュの記法修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
 
   devise_scope :user do
-    get "signup", :to => "users/registrations#new"
-    get "login", :to => "users/sessions#new"
-    get "logout", :to => "users/sessions#destroy"
-    post "users/guest_sign_in", to: "users/sessions#new_guest"
+    get "signup" => "users/registrations#new"
+    get "login" => "users/sessions#new"
+    get "logout" => "users/sessions#destroy"
+    post "users/guest_sign_in" => "users/sessions#new_guest"
   end
 
   root 'static_pages#home'
