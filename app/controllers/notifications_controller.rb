6fb453c2664c94d8d3b7c84c8ab368a3daa2df0e
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2021-02-02 23:04:50 +0900

    ログイン中のみ通知一覧を表示できるようにする

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,6 @@
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @notifications = current_user.passive_notifications.where.not(visiter_id: current_user.id)
     @notifications = @notifications.page(params[:page]).per(20)
