1d0172a0bbf349b6c9e31a73840d2b33e830f620
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2021-02-02 17:18:10 +0900

    いいね通知の作成メソッド修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -32,7 +32,7 @@ class Post < ApplicationRecord
 
   def create_notification_like!(current_user)
     # すでに「いいね」済みか検索
-    temp = notification.where(["visiter_id = ? and visited_id = ? and post_id = ? and action = ?", current_user.id, user_id, id, 'like'])
+    temp = Notification.where(["visiter_id = ? and visited_id = ? and post_id = ? and action = ?", current_user.id, user_id, id, 'like'])
     # いいねされていない場合のみ、通知を新規作成
     if temp.blank?
       notification = current_user.active_notifications.new(
@@ -55,6 +55,21 @@ class Post < ApplicationRecord
       save_notification_comment!(current_user, comment_id, temp_id['user_id'])
     end
     # まだ誰もコメントしていない場合は、投稿者に通知を送る
-    save_notification_comment!(current_user, comment_id, visited_id)
+    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+  end
+
+  def save_notification_comment!(current_user, comment_id, visited_id)
+    # コメントは複数回することが考えられるため、1つの投稿に複数回通知する
+    notification = current_user.active_notifications.new(
+      post_id: id,
+      comment_id: comment_id,
+      visited_id: visited_id,
+      action: 'comment'
+    )
+    # 自分のコメントの場合は、通知済みとする
+    if notification.visiter_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
   end
 end
