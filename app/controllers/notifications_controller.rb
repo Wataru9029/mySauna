9f8cb7f85ce4ebd8be829fbf5038acffcefb5f6e
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2021-02-02 22:54:15 +0900

    通知一覧ページ作成

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,7 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.passive_notifications.page(params:[:page]).per(20)
+    @notifications = current_user.passive_notifications.where.not(visiter_id: current_user.id)
+    @notifications = @notifications.page(params[:page]).per(20)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
     end
