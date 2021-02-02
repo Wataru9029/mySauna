4a73c133144e7d367cbc2d857a5ccebe627b93a0
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2021-02-02 17:06:28 +0900

    いいね通知の作成メソッド

--- app/models/post.rb
+++ app/models/post.rb
@@ -29,4 +29,22 @@ class Post < ApplicationRecord
 
     Post.where(['title LIKE ? OR description LIKE ?', "%#{search}%", "%#{search}%"])
   end
+
+  def create_notification_like!(current_user)
+    # すでに「いいね」済みか検索
+    temp = notification.where(["visiter_id = ? and visited_id = ? and post_id = ? and action = ?", current_user.id, user_id, id, 'like'])
+    # いいねされていない場合のみ、通知を新規作成
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        post_id: id,
+        visited_id: user_id,
+        action: 'like'
+      )
+    end
+    # 自分の投稿に対するいいねの場合は通知済みになる
+    if notification.visiter_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
+  end
 end
