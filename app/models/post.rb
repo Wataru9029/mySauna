583586726266c08a52aae9ecaa9f0b8e89163389
伊藤渉 <wataru@itouwatarunoMacBook-Air.local>
2021-02-02 16:47:37 +0900

    モデル間の関連付け

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
+  has_many :notifications, dependent: :destroy
 
   mount_uploader :image, ImageUploader
 
