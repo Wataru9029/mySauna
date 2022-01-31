ff5f43adcd6d5ef3f61bdf8a2ada7029b3696ebb
Wataru9029 <itodesu0621@gmail.com>
2022-01-31 19:31:05 +0900

    Rails6バージョンアップに伴うメソッド名修正

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe '静的ページ', type: :request do
   context 'トップページ' do
     it 'レスポンスが正常に表示されること' do
       get root_path
-      expect(response).to be_success
+      expect(response).to be_successful
       expect(response).to have_http_status '200'
     end
   end
